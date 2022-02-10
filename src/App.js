e073e73658afc6c1150d53dcfc5c612b2fabc25b
FelipeloDev <felipe.londono@pragma.com.co>
2022-02-10 10:37:54 -0500

    authenticating the token to give way to the home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getTokenFromResponse } from './components/atoms/spotifyData';
+import Login from './components/pages/Login';
 
 function App() {
+
+  const [token, setToken]=useState(null);
+
+
+  useEffect( ()=>{
+      const token = getTokenFromResponse();
+      window.location.hash = "";
+      const tempToken = hash.accesstempToken;
+
+      if (tempToken){
+        setToken(tempToken)
+      }
+
+    },[]  );
+
+
+
   return (
-    <div className="App">
-      hello
+    <div className="app">
+      {
+        token ? (
+          <h2>Logged In</h2>       
+        ):(
+          <Login/>
+        )
+      }
+      
     </div>
   );
 }
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Login.css";
+import { accessUrl } from "../atoms/spotifyData";
 
 function Login() {
     return (
